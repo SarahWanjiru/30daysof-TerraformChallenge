448369b485ea10debf508e85a76e25fd47f06c1e
SarahWanjiru <muhiawanjiru4@gmail.com>
2026-04-15 20:59:23 +0300

    feat(day26): add initial Terraform configuration for scalable web app including EC2, ALB, and ASG modules

---
+++ day26-scalable-web-app/modules/ec2/main.tf
@@ -0,0 +1,36 @@
+locals {
+    common_tags = {
+        Environment = var.environment
+        ManagedBy = "Terraform"
+        Project = "scalable-web-app"
+    }
+}
+
+resource "aws_security_group" "instancesg" {
+    name = " web-instance-sg-${var.environment}"
+    description = "Allow HTTP/HTTPS inbound to EC2 instances"
+
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+      ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+      egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+
---
+++ day26-scalable-web-app/modules/ec2/variables.tf
@@ -0,0 +1,35 @@
+variable "instance_type" {
+    description = "The type of EC2 instance to use"
+    type = string
+    default = t3.micro
+}
+
+
+variable "ami_id" {
+    description = "The ID of the AMI to use for the EC2 instance"
+    type = string
+}
+
+variable "key_name" {
+    description = "The name of the key pair to use for SSH access to the EC2 instance"
+    type = string
+    default = null
+}
+
+variable "environment" {
+    description = "Deployment environment (e.g., dev, staging, prod)"
+    type = string
+    
+    validation {
+        condition = contains(["dev", "staging", "prod"], var.environment)
+        error_message = "Environment must be one of 'dev', 'staging', or 'prod'."
+    }
+}
+
+variable "tags" {
+    description = "Additional tags to apply to all resources"
+    type = map(string)
+    default = {}
+}
+
+
